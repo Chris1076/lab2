3e4de12b7714514c2de047cc25a9d48a979c1863
Chris1076 <iamchristian28@gmail.com>
2024-08-26 21:34:07 +0800

    Done with instructions.php and the agree checkbox added JS handling

--- lab3a/scripts.js
+++ lab3a/scripts.js
@@ -2,6 +2,13 @@ var emailregex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 function bodyload(){
     document.getElementById('submitBtn').disabled = true;
 }
+function checkTerms(state){
+    if (state.checked == true){
+        document.getElementById('submitBtn').disabled = false;
+    }else{
+        document.getElementById('submitBtn').disabled = true;
+    }
+}
 function ontype(){
     var compname = document.forms["indexform"]["completename"].value;
     var email = document.forms["indexform"]["email"].value;
