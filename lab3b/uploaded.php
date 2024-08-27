d99535300c4742b503120a43a322bff4b6d5598b
Chris1076 <iamchristian28@gmail.com>
2024-08-27 20:24:05 +0800

    Branched to pdf branch and made some edits

--- lab3b/uploaded.php
+++ lab3b/uploaded.php
@@ -1,22 +1,33 @@
 <?php
-
+if (isset($_FILES['pdf_file'])) {
 $upload_directory = getcwd() . '/uploads/';
 $relative_path = '/uploads/';
 
 // Handle Text File
-$uploaded_text_file = $upload_directory . basename($_FILES['text_file']['name']);
-$temporary_file = $_FILES['text_file']['tmp_name'];
+$file_name = $_FILES['jpg_file']['name'];
+$uploaded_text_file = $upload_directory . basename($_FILES['pdf_file']['name']);
+$temporary_file = $_FILES['pdf_file']['tmp_name'];
+}?>
 
+<html>
+  <head>
+    <title>PDF Example by Object Tag</title>
+  </head>
+  <body>
+    <h1>PDF Example by Object Tag</h1>
+<?php
 if (move_uploaded_file($temporary_file, $uploaded_text_file)) {
     $text_file_content = file_get_contents($uploaded_text_file, 'r');
     ?>
-    <textarea cols="70" rows="30"><?php echo $text_file_content; ?></textarea>
+    <object data="<?php ?>" type="application/pdf" width="100%" height="500px">
     <?php
 } else {
     echo 'Failed to upload file';
 }
-
-
-echo '<pre>';
+?>
+ </object>
+  </body>
+</html>
+<!-- echo '<pre>';
 var_dump($_FILES);
-exit;
+exit; -->
