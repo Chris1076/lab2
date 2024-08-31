51b0e8ce9764838f3baeb678d2b5cca09d5382ce
Chris1076 <iamchristian28@gmail.com>
2024-08-31 20:02:38 +0800

    Finished PDF branch

--- lab3b/uploaded.php
+++ lab3b/uploaded.php
@@ -4,8 +4,8 @@ $upload_directory = getcwd() . '/uploads/';
 $relative_path = '/uploads/';
 
 // Handle Text File
-$file_name = $_FILES['jpg_file']['name'];
-$uploaded_text_file = $upload_directory . basename($_FILES['pdf_file']['name']);
+$file_name = $_FILES['pdf_file']['name'];
+$uploaded_text_file = $upload_directory . basename($file_name);
 $temporary_file = $_FILES['pdf_file']['tmp_name'];
 }?>
 
@@ -19,10 +19,11 @@ $temporary_file = $_FILES['pdf_file']['tmp_name'];
 if (move_uploaded_file($temporary_file, $uploaded_text_file)) {
     $text_file_content = file_get_contents($uploaded_text_file, 'r');
     ?>
-    <object data="<?php ?>" type="application/pdf" width="100%" height="500px">
+    <object data="<?php echo $uploaded_text_file;?>" type="application/pdf" width="100%" height="500px">
+    <p>Unable to display PDF file. <a href="<?php echo $uploaded_text_file;?>" target="_blank" download>Download</a> instead.</p>
     <?php
 } else {
-    echo 'Failed to upload file';
+    echo '<p>Failed to upload file</p>';
 }
 ?>
  </object>
