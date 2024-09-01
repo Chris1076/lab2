94807c6fc7542bb182deab0ec6a53c936f4a7e63
Chris1076 <iamchristian28@gmail.com>
2024-09-01 14:35:37 +0800

    Final adjustments to pdf branch

--- lab3b/uploaded.php
+++ lab3b/uploaded.php
@@ -7,26 +7,28 @@ $relative_path = '/uploads/';
 $file_name = $_FILES['pdf_file']['name'];
 $uploaded_text_file = $upload_directory . basename($file_name);
 $temporary_file = $_FILES['pdf_file']['tmp_name'];
+$server_path = $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"] . $relative_path . basename($file_name);
 }?>
 
 <html>
   <head>
-    <title>PDF Example by Object Tag</title>
+    <title>IPT10 Lab activity 3b</title>
   </head>
   <body>
     <h1>PDF Example by Object Tag</h1>
 <?php
 if (move_uploaded_file($temporary_file, $uploaded_text_file)) {
+    $correct_directory = str_replace('\\', '/', $server_path);
     $text_file_content = file_get_contents($uploaded_text_file, 'r');
     ?>
-    <object data="<?php echo $uploaded_text_file;?>" type="application/pdf" width="100%" height="500px">
-    <p>Unable to display PDF file. <a href="<?php echo $uploaded_text_file;?>" target="_blank" download>Download</a> instead.</p>
+    <object data="<?php echo "http://$correct_directory";?>" type="application/pdf" width="100%" height="500px">
+    <p>Unable to display PDF file. <a href="<?php echo "http://$correct_directory";?>" target="_blank" download>Download</a> instead.</p>
+    </object>
     <?php
 } else {
     echo '<p>Failed to upload file</p>';
 }
 ?>
- </object>
   </body>
 </html>
 <!-- echo '<pre>';
