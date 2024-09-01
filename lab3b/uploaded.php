b623979e527ea6711a58c87a098bcf44d8683a95
Chris1076 <iamchristian28@gmail.com>
2024-09-01 14:28:51 +0800

    Finished audio file

--- lab3b/uploaded.php
+++ lab3b/uploaded.php
@@ -5,22 +5,26 @@ $relative_path = '/uploads/';
 
 // Handle Text File
 $file_name = $_FILES['audio_file']['name'];
-$uploaded_text_file = $upload_directory . basename($file_name);
+$uploaded_audio_file = $upload_directory . basename($file_name);
 $temporary_file = $_FILES['audio_file']['tmp_name'];
+$server_path = $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"] . $relative_path . basename($file_name);
 }?>
 
 <html>
   <head>
-    <title>PDF Example by Object Tag</title>
+    <title>IPT Lab Activity 3b</title>
   </head>
   <body>
-    <h1>PDF Example by Object Tag</h1>
+    <h1>Audio file via audio tag</h1>
 <?php
-if (move_uploaded_file($temporary_file, $uploaded_text_file)) {
-    $text_file_content = file_get_contents($uploaded_text_file, 'r');
+if (move_uploaded_file($temporary_file, $uploaded_audio_file)) {
+    $correct_directory = str_replace('\\', '/', $server_path);
+    // $text_file_content = file_get_contents($uploaded_text_file, 'r');
     ?>
-    <object data="<?php echo $uploaded_text_file;?>" type="application/pdf" width="100%" height="500px">
-    <p>Unable to display PDF file. <a href="<?php echo $uploaded_text_file;?>" target="_blank" download>Download</a> instead.</p>
+    <audio controls autoplay name="media">
+      <source src="<?php echo "http://$correct_directory";?>" type="audio/mpeg">
+      Your browser does not support the audio element.
+    </audio>
     <?php
 } else {
     echo '<p>Failed to upload file</p>';
